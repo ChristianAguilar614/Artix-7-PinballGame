8addd9342ebc1612c3149bdd9b4799d995127898
Justin Oroz <justin.oroz@me.com>
2017-05-18 14:32:47 -0700

    Edge Detection

--- final_pinball.srcs/sources_1/new/ball.v
+++ final_pinball.srcs/sources_1/new/ball.v
@@ -13,10 +13,10 @@
 
 localparam [7:0] initialx = 8'd230;
 localparam [7:0] initialy = 8'd100;
-localparam signed [7:0] initialdy = 0;
-localparam [3:0] max_velo = 4'd6;
-localparam [1:0] gravity_magnitude = 2'd1;
-localparam [3:0] gravity_rate = 4'd1;
+localparam signed [7:0] initialdy = -6;
+localparam [3:0] max_velo = 4'd10;
+localparam [1:0] gravity_magnitude = 2'd2;
+localparam [3:0] gravity_rate = 4'd4;
 
 module ball(
 	input wire pclk,
@@ -56,7 +56,7 @@ reg [7:0] storey;
 
 // Status
 reg collision = 0;
-reg [3:0] situation = 0;
+wire [3:0] situation;
 
 // Counters
 
@@ -67,13 +67,35 @@ reg [3:0] gravity_ctr = 0;
 always @(posedge pclk)
 begin
 	case(step)
-		2'd0: // save current position for calculations. Check current location for collision
-		begin
+		2'd0: // React to collision, modify velocity accordingly
+		begin		
 			if (!reset)
 			begin
-				situation <= SITUATION(posx, posy);
 				storex <= posx;
 				storey <= posy;
+				
+				case (situation) // left, right, top, bottom
+					4'b1000, 4'b0100:  // left, right
+					begin
+						dx <= (!collision) ? dx * -1: dx;
+						collision <= 1;
+					end
+					4'b0010, 4'b0001, 4'b1110, 4'b1101:  // top, bottom, odd case when both sides and top hit
+					begin
+						dy <= (!collision) ? dy * -1: dy;
+						collision <= 1;
+					end
+					4'b1010, 4'b1001, 4'b0110, 4'b0101: // Corners
+					begin
+						dx <= (!collision) ? dx * -1: dx;
+						dy <= (!collision) ? dy * -1: dy;
+						collision <= 1;
+					end
+					
+					default: collision <= 0;
+					
+				endcase
+			
 			end
 			else // Handle a reset
 			begin 
@@ -81,62 +103,10 @@ begin
 				storey <= initialy;
 				dx <= 0;
 				dy <= initialdy;
-				situation <= 4'b0000;
-			end
+			end 
 			step <= step + 1;
 		end
-		2'd1: // React to collision, modify velocity accordingly
-		begin
-			// save current position
-			
-			case (situation) // left, right, top, bottom
-				4'b1000, 4'b0100:  // left, right
-				begin
-						
-					collision <= 1;
-				end
-				4'b0010, 4'b0001, 4'b1110, 4'b1101:  // top, bottom, odd case when both sides and top hit
-				begin
-					dy <= (!collision) ? dy * -1: dy;
-					collision <= 1;
-				end
-				4'b1010: // Left Top
-				begin
-					collision <= 1;
-				end
-				4'b1001: // Left Bottom
-				begin
-					collision <= 1;
-				end
-				4'b0110: // Right Top
-				begin
-					collision <= 1;
-				end
-				4'b0101: // Right Bottom
-				begin
-					collision <= 1;
-				end
-				
-				default: collision <= 0;
-					
-			endcase
-			
-				/*
-				if (posy < 20 || posy > 230)
-				begin
-					// only apply on first time collision is detected
-					dy <= (!collision) ? dy * -1: dy;
-					collision <= 1;
-				end
-				else collision <= 0; */
-				
-				// if posy > Blah do stuff
-				// (dy < 0) storey <= storey - (dy * -1): storey + dy;
-				// handles signed 
-			
-			step <= step + 1;
-		end
-		2'd2: // apply gravity
+		2'd1: // apply gravity
 		begin
 			if (gravity_ctr == gravity_rate)
 			begin // only accelerate if under max speed
@@ -146,7 +116,7 @@ begin
 					dy <= (dy > 0) ? dy + gravity_magnitude : -1 * ((dy * -1) - gravity_magnitude);
 				end
 				else dy <= dy;
-				gravity_ctr <= 0;
+				gravity_ctr <= 0; // reset counter
 			end
 			else gravity_ctr <= gravity_ctr + 1;
 			
@@ -154,41 +124,45 @@ begin
 			
 			step <= step + 1;
 		end
-		2'd3: // update location
+		2'd2: // update location
 		begin
 			newx <= storex + dx;
 			newy <= storey + dy;
 			step <= 2'd0; // back to start
 		end
-		default: ;
+		default: step <= 2'd0; // back to start;
 	endcase
 end
 
 /******************************* functions ******************/    
 
+assign situation[3] = (posx < 20); // left
+assign situation[2] = (posx > 240); // right
+assign situation[1] = (posy < 20); // top
+assign situation[0] = (posy > 240); // bottom
 
 function [3:0] SITUATION;
 input topLX, topLY; 
-	SITUATION = {CHECK_LEFT(topLX, topLY), CHECK_RIGHT(topLX, topLY), CHECK_UPPER(topLX, topLY), CHECK_LOWER(topLX, topLY)};
+	SITUATION = {CHECK_LEFT(topLX), CHECK_RIGHT(topLX), CHECK_UPPER(topLY), CHECK_LOWER(topLY)};
 endfunction
     
 function [0:0] CHECK_UPPER; // 0010
-input topLX, topLY;
+input topLY;
 	CHECK_UPPER = (topLY < 20);
 endfunction
 
 function [0:0]CHECK_LOWER; // 0001
-input topLX, topLY;
+input topLY;
 	CHECK_LOWER = (topLY > 230);
 endfunction
 
 function [0:0] CHECK_LEFT; //1000
-input topLX, topLY;
+input topLX;
 	CHECK_LEFT = (topLX <= size);
 endfunction
 
 function [0:0] CHECK_RIGHT; //0100
-input topLX, topLY;
+input topLX;
 	CHECK_RIGHT = ((topLX + size) >= (8'd255 - size));
 	endfunction
     
